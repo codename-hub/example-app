ad5c12135f48f5b65f2e6905e9ff4b1b597681a2
Kevin <kevin.dargel@onlinehome.de>
2021-11-29 15:33:25 +0100

    added /start/exampleform

--- backend/class/context/start.php
+++ backend/class/context/start.php
@@ -27,4 +27,59 @@ class start extends \codename\core\context
 
     $this->getResponse()->setData('dataset', $dataset);
   }
+
+  /**
+   * [view_exampleform description]
+   */
+  public function view_exampleform(): void {
+    $form = new \codename\core\ui\form([
+      'form_id' => 'example',
+      'form_method' => 'POST',
+      'form_action' => '',
+    ]);
+
+    $form->addField(new \codename\core\ui\field([
+      'field_type'  => 'input',
+      'field_title' => 'Simple text input',
+      'field_name'  => 'simple_text_input',
+    ]));
+
+    $form->addField(new \codename\core\ui\field([
+      'field_type'      => 'input',
+      'field_title'     => 'Simple number input',
+      'field_datatype'  => 'number_natural',
+      'field_name'      => 'simple_number_input',
+    ]));
+
+    $form->addField(new \codename\core\ui\field([
+      'field_type'      => 'input',
+      'field_required'  => true,
+      'field_title'     => 'Simple required email input',
+      'field_datatype'  => 'text_email',
+      'field_name'      => 'simple_required_input',
+    ]));
+
+    $form->addField(new \codename\core\ui\field([
+      'field_type'  => 'submit',
+      'field_title' => 'Submit',
+      'field_value' => 'Submit',
+      'field_name'  => 'some_submit_field',
+    ]));
+
+    if($form->isSent()) {
+      $this->getResponse()->setData('recent_formdata_valid', $form->isValid());
+
+      // reset errorstack, as a call to ->isValid() will add messages to the errorstack
+      $form->getErrorstack()->reset();
+
+      if(!$form->isValid()) {
+        $this->getResponse()->setData('recent_formdata_errors', $form->getErrorstack()->getErrors());
+      }
+
+      $this->getResponse()->setData('recent_formdata', $form->getData());
+      $this->getResponse()->setData('recent_formdata_normalized', $form->normalizeData($form->getData()));
+    }
+
+    $this->getResponse()->setData('form', $form->output());
+  }
 }
